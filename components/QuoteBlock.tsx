4df5f8dcfabae4269115721dd213e9b21892e3fd
Jamie <77027304+itisjamied@users.noreply.github.com>
2025-04-02 18:05:07 -0400

    responsive flexbox
    Still need to make spacing tweaks

--- components/QuoteBlock.tsx
+++ components/QuoteBlock.tsx
@@ -9,12 +9,12 @@ interface QuoteBlockProps {
 
 const QuoteBlock: React.FC<QuoteBlockProps> = ({  quote, author, authorImageSrc }) => {
   return (
-    <div className="w-full flex flex-col items-start gap-4 pt-20 pb-10 px-4">
+    <div className="w-full flex flex-col md:flex-row items-start md:items-center gap-4 pt-20 md:pt-10 pb-10 px-4">
       {/* Quote Symbol */}
       <Image src="./quote.svg" alt="Quote Symbol" width={140} height={110} className="" />
-
-      <p className="text-left text-[20px] text-white italic font-semibold leading-none [text-edge:cap] [leading-trim:both] mt-2">{quote}</p>
-      <span className="text-[16px] text-white italic font-semibold leading-none uppercase mt-2">{author}</span>
+<div>  <p className="text-left text-[20px] text-white italic font-semibold leading-none [text-edge:cap] [leading-trim:both] mt-2">{quote}</p>
+<span className="text-[16px] text-white italic font-semibold leading-none uppercase mt-2">{author}</span></div>
+    
       <div className="flex items-center justify-center gap-4 w-full">
         <Image
           src={authorImageSrc}
