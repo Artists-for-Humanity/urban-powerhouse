7b484a91bd7853da187838e0477b8921cd1dc51b
Jamie <77027304+itisjamied@users.noreply.github.com>
2025-04-08 17:04:27 -0400

    Turned it into carousel

--- components/ImageBlock.tsx
+++ components/ImageBlock.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import Image from 'next/image';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
 
 interface ImageBlockProps {
   images: string[];
@@ -12,37 +16,29 @@ const ImageBlock: React.FC<ImageBlockProps> = ({ images, className = '' }) => {
   return (
     <div className={`grid grid-cols-subgrid col-span-full ${className}`}>
       {/* Mobile Layout */}
-      <div className="grid col-span-full gap-y-[4px] md:hidden">
-        {images.slice(0, 4).map((src, index) => (
-          <div key={index} className="col-span-full">
-            <Image
-              src={src}
-              alt={`Image ${index + 1}`}
-              width={400}
-              height={300}
-              className="w-full h-auto object-cover"
-            />
-          </div>
+ 
+    <div className="col-span-full lg:hidden">
+      <Swiper
+        modules={[Pagination]}
+        pagination={{ clickable: true }}
+        spaceBetween={10}
+        slidesPerView={1}
+      >
+        {images.slice(0, 10).map((src, index) => (
+          <SwiperSlide key={index}>
+           <Image
+          src={src}
+          alt={`Image ${index + 1}`}
+          width={376} 
+          height={282} 
+          className="w-[376px] md:w-[793px] md:h-[596px] h-[282px] object-cover mx-auto"
+        />
+          </SwiperSlide>
         ))}
-      </div>
+      </Swiper>
+    </div>
 
-      {/* Tablet Layout */}
-      <div className="hidden md:grid col-start-2 col-span-6 grid-cols-subgrid md:grid-cols-2 md:gap-4 lg:hidden">
-        {images.slice(0, 9).map((src, index) => (
-          <div
-            key={index}
-            className={index === 0 ? 'col-span-full' : 'col-span-1'} 
-          >
-            <Image
-              src={src}
-              alt={`Image ${index + 1}`}
-              width={400}
-              height={300}
-              className="w-full h-full object-cover"
-            />
-          </div>
-        ))}
-      </div>
+ 
 
       {/* Desktop Layout */}
       
