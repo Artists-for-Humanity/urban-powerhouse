ae113e6f851710e32a89966f6d2ff1f67eab9891
Jamie <77027304+itisjamied@users.noreply.github.com>
2025-04-08 23:47:02 -0400

    Adjusted quote block spacing

--- components/QuoteBlock.tsx
+++ components/QuoteBlock.tsx
@@ -9,11 +9,11 @@ interface QuoteBlockProps {
 
 const QuoteBlock: React.FC<QuoteBlockProps> = ({  quote, author, authorImageSrc }) => {
   return (
-    <div className="grid col-span-full grid-cols-subgrid gap-y-[32px]">
+    <div className="grid col-span-full grid-cols-subgrid gap-y-[32px] lg:py-10">
 
-      <Image src="./quote.svg" alt="Quote Symbol" width={140} height={110} className="col-span-3 lg:col-span-2 mx-[32px] lg:mx-0 lg:w-[203px] lg:h-[161px]" />
+      <Image src="./quote.svg" alt="Quote Symbol" width={140} height={110} className="col-span-3 lg:col-span-2 mx-[32px] lg:mx-0 lg:w-[203px] lg:h-[161px] lg:mt-6" />
 
-      <div className='col-start-1 col-span-full lg:col-start-4 lg:col-span-6  mx-[32px] lg:mx-0'>  
+      <div className='col-start-1 col-span-full lg:col-start-4 lg:col-span-6  mx-[32px] lg:mx-0 lg:p-y-10 lg:flex lg:flex-col lg:justify-center'>  
         <p className="text-left text-[20px] md:text-[24px] lg:text-[32px] text-white italic font-semibold leading-none [text-edge:cap] [leading-trim:both] mt-2 mb-[24px]">{quote}</p>
         <span className="text-[16px] lg:text-[20px] text-white italic font-semibold leading-none uppercase mt-2">{author}</span>
       </div>
