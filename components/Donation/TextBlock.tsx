f2ce7c508053f73e9666850de29cea613bc3005e
Jamie <77027304+itisjamied@users.noreply.github.com>
2025-04-15 17:15:58 -0400

    remove orange bar on donation pargraphs

--- components/Donation/TextBlock.tsx
+++ components/Donation/TextBlock.tsx
@@ -14,10 +14,7 @@ const TextBlock: React.FC<TextBlockProps> = ({ text, className = '' }) => {
           text-base font-semibold pl-6 sm:pl-10 relative col-span-full sm:col-start-2 sm:col-span-6 lg:col-start-3 lg:col-span-8
         `}
       >
-        <span
-          className="absolute left-0 top-0 lg:top-1/2 lg:-translate-y-1/2 h-[90px] w-[12px] bg-[var(--urban-orange)]"
-          aria-hidden="true"
-        ></span>
+
         {text}
       </h1>
     </div>
