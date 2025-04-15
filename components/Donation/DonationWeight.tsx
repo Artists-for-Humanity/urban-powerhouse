ede78659d8c3275b6ba0926612ba6352cb39f514
Jamie <77027304+itisjamied@users.noreply.github.com>
2025-04-15 16:30:59 -0400

    Weight hover/select effect

--- components/Donation/DonationWeight.tsx
+++ components/Donation/DonationWeight.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import Image from 'next/image';
+import { useState } from 'react';
+// import Image from 'next/image';
 
 interface DonationItem {
   amount: string;
@@ -13,6 +14,7 @@ interface DonationWeightProps {
 }
 
 const DonationWeight: React.FC<DonationWeightProps> = ({ title, donations, className = '' }) => {
+  const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
   return (
     <div className={`col-span-full ${className} grid grid-cols-subgrid`}>
       <h1 className="hidden lg:block col-start-3 col-span-8 text-2xl text-center font-bold mb-8">
@@ -40,19 +42,45 @@ const DonationWeight: React.FC<DonationWeightProps> = ({ title, donations, class
                 : ''
             }`}
             >
-              <div className="relative w-full h-[150px] flex justify-center items-center">
-                <Image
-                  height={64}
-                  width={71}
-                  src="/weight.svg"
-                  alt="Weight Background"
-                  className="absolute inset-0 w-full h-full object-contain rounded-lg"
-                />
-                <span className="relative text-xl font-bold z-10 mt-12 mr-4">
-                  {donation.amount}
-                </span>
-              </div>
-
+     
+            <div
+              className={`relative w-full h-[150px] flex justify-center items-center group`}
+              onClick={() => setSelectedIndex(index)} // Set the selected index on click
+            >
+            <svg
+              width="143"
+              height="128"
+              viewBox="0 0 143 128"
+              xmlns="http://www.w3.org/2000/svg"
+              className={`cursor-pointer absolute inset-0 w-full h-full object-contain rounded-lg filter drop-shadow-[11px_4px_0_rgba(255,94,0,1)] ${
+              selectedIndex === index
+                ? 'drop-shadow-none translate-x-[11px] translate-y-[4px]'
+                : 'group-hover:drop-shadow-none group-hover:translate-x-[11px] group-hover:translate-y-[4px]'
+              } transition-300 transition-all`}
+            >
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+                d="M47.6256 47C43.9533 42.0932 41.7783 36.0009 41.7783 29.4005C41.7783 13.1631 54.9414 0 71.1788 0C87.4163 0 100.579 13.1631 100.579 29.4005C100.579 36.0009 98.4044 42.0932 94.732 47H105.243C113.261 47 120.505 51.7895 123.645 59.1679L140.955 99.8401C146.569 113.032 136.889 127.672 122.552 127.672H20.4478C6.08718 127.672 -3.59342 112.987 2.06705 99.7888L19.5109 59.1167C22.6637 51.7656 29.8931 47 37.8917 47H47.6256ZM67.7478 47C59.4881 45.3992 53.2515 38.1283 53.2515 29.4008C53.2515 19.4999 61.2777 11.4736 71.1786 11.4736C81.0795 11.4736 89.1058 19.4999 89.1058 29.4008C89.1058 38.1283 82.8691 45.3992 74.6094 47H67.7478Z"
+              className={`${
+                selectedIndex === index
+                  ? 'fill-[var(--urban-orange)]'
+                  : index === donations.length - 1
+                  ? 'fill-[var(--urban-grey)]'
+                  : 'fill-[var(--urban-blue)] group-hover:fill-[var(--urban-orange)]'
+              } transition-colors duration-300`}
+            />
+            </svg>
+            <span
+              className={`relative text-xl font-bold z-10 mt-12 cursor-pointer ${
+              selectedIndex === index
+                ? 'translate-x-[11px] translate-y-[4px]'
+                : 'group-hover:translate-x-[11px] group-hover:translate-y-[4px]'
+              } transition-300 transition-all`}
+            >
+            {donation.amount}
+            </span>
+            </div>
               <p className="text-base font-semibold mt-4 text-center">
                 {donation.description}
               </p>
@@ -60,7 +88,6 @@ const DonationWeight: React.FC<DonationWeightProps> = ({ title, donations, class
           );
         })}
       </div>
-      {/* <Button label="Donate" href="/" variant="default" /> */}
       
     </div>
   );
