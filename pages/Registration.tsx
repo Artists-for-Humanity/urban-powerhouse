7c8ff0085157b125b004717d0850bc20e1d72e4b
Vinh <145184639+v-iinh@users.noreply.github.com>
2025-04-22 17:45:57 -0400

    tested compact form, fixed width

--- pages/Registration.tsx
+++ pages/Registration.tsx
@@ -19,12 +19,7 @@ export default function Registration() {
     if (widgetRef.current) {
       widgetRef.current.insertAdjacentHTML(
         'beforeend',
-        `<healcode-widget 
-            data-type="registrations" 
-            data-widget-partner="object" 
-            data-widget-id="841598651f60" 
-            data-widget-version="0">
-         </healcode-widget>`
+        `<healcode-widget data-type="registrations" data-widget-partner="object" data-widget-id="841599951f60" data-widget-version="0" ></healcode-widget>`
       );
     }
   }, []);
@@ -33,18 +28,18 @@ export default function Registration() {
     <Grid>
       <Navigation />
       <Container>
-        <h1 className="text-[32px] font-semibold col-span-full">Registration & Membership</h1>
+        <h1 className="text-[32px] font-semibold col-span-full">Registration & Membership</h1><br />
         <h3 className="font-semibold col-span-full">
           Welcome to Urban PowerHouse, Boston’s home for powerlifters and olympic weightlifters. 
           We are more than just a place to train, we are a nonprofit gym on a mission. 
           Our goal is to get a barbell into the hands of as many people as possible, 
           from all walks of life, and to provide a supportive community environment for 
           strength sports athletes living in and around the Greater Boston area.
-        </h3>
-        <h3 className="font-semibold col-span-full">Register today to get started.</h3>
+        </h3><br />
+        <h3 className="font-semibold col-span-full">Register today to get started.</h3><br />
 
         {}
-        <div ref={widgetRef} className="w-full mt-8" />
+        <div ref={widgetRef} className="col-span-full" />
 
       </Container>
       <Footer />
