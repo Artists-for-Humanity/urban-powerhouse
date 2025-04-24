7c24f97e52ebba4467cdff1a7c59d0f03615e535
Jamie <77027304+itisjamied@users.noreply.github.com>
2025-04-24 18:02:45 -0400

    Added funs article

---
+++ pages/Articles/ZakimFund.tsx
@@ -0,0 +1,33 @@
+"use client";
+import React from "react";
+import Grid from "../../components/GridContainer";
+import Footer from "../../components/Footer";
+import Container from "../../components/Container";
+import Navigation from "../../components/Navigation";
+import ArticleBlock from "../../components/News/ArticleBlock";
+import '../../app/globals.css';
+
+
+
+export default function News() {
+
+    return (
+          <Grid>
+            <Navigation/>
+              <Container className="">
+              <h1 className="text-[32px] font-semibold col-span-full mb-[40px] ">News & Events</h1>
+              <ArticleBlock variant="title" title="Urban PowerHouse Named 2025 Zakim Fund Grantee" />
+            <ArticleBlock
+            variant="paragraph"
+            paragraphs={[
+                "Urban PowerHouse is proud to have been named a 2025 Grantee Partner of The Lenny Zakim Fund. The Lenny Zakim Fund works to develop deep relationships and builds bridges among people and communities to advance social, economic, and racial justice.",
+                "“We are honored to be a part of the Zakim Fund portfolio and look forward to the unique opportunities for partnership, collaboration, and learning that are to come. The Zakim Fund supports fantastic organizations doing impactful work in our communities, and we are so grateful to work alongside these changemakers.” — Joseph Stephen, Executive Director & Co-Founder"
+            ]}
+            />
+            <ArticleBlock variant="go-back" />
+              </Container>
+            <Footer />
+          </Grid>
+
+    );
+}
--- pages/News.tsx
+++ pages/News.tsx
@@ -54,7 +54,7 @@ export default function News() {
         "Urban PowerHouse is proud to have been named a 2025 Grantee Partner of The Lenny Zakim Fund. The Lenny Zakim Fund works to develop deep relationships and builds bridges among people and communities to advance social, economic, and racial justice.",
         "“We are honored to be a part of the Zakim Fund portfolio and look forward to the unique opportunities for partnership, collaboration, and learning that are to come. The Zakim Fund supports fantastic organizations doing impactful work in our communities, and we are so grateful to work alongside these changemakers.” — Joseph Stephen, Executive Director & Co-Founder"
       ],
-      link: "/Article",
+      link: "/Articles/ZakimFund",
       variant: "pinned"
     },
     {
