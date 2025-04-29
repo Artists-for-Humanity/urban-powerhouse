d7c575265f2147bc748c2e71c10ebcd5d4a0261a
Jamie <77027304+itisjamied@users.noreply.github.com>
2025-04-29 11:32:37 -0400

    donation typo and syntax updates

--- pages/Donation.tsx
+++ pages/Donation.tsx
@@ -22,18 +22,18 @@ export default function Donation() {
             <Navigation/>
               <Container>
                 <h1 className="text-[32px] font-semibold col-span-full ">Why Give to UPH?</h1>
-                <TextBlock text="Urban PowerHouse will create the first nonprofit strength-training facility supporting underserved populations in Boston and neighboring areas. Help us make powerlifting and weightlifting attainable for everyone by making a gift today. Your donation will help us provide training, information access and financial support to youth and adult athletes." />
+                <TextBlock text="Urban PowerHouse has created one the first nonprofit strength-training facilities supporting underserved populations in Boston and neighboring areas. Help us make strength sports, and health and wellness attainable for everyone by making a gift today. Your donation will help us provide training, information access and financial support to youth and adult athletes." />
                 <DonationWeight
-                    title="Your Donation Counts Help under represented teens access the world of strength training!"
+                    title="Your Donation Counts Help underrepresented youth, adults, and families access the world of strength training!"
                     donations={[
-                    { amount: "$30", description: "Become a Friend of UPH " },
-                    { amount: "$65", description: "Supports a 12 month federation membership for one athlete" },
+                    { amount: "$30", description: "Become a Friend of UPH. " },
+                    { amount: "$65", description: "Supports a 12 month federation membership for one athlete." },
                     { amount: "$100", description: "Covers one competition entry fee for one athlete." },
                     { amount: "$250", description: "Covers approved competition gear for one athlete." },
                     { amount: "$600", description: "Purchases a new barbell for athletes to use during training." },
-                    { amount: "$1000", description: "Helps cover travel cost associated with larger scale competitions." },
-                    { amount: "$2500", description: "Helps us open the doors to our first brick and mortar facility" },
-                    { amount: "$__", description: "Make a gift. Every donation, no matter the size, will help us make Boston strong  together" },
+                    { amount: "$1000", description: "Helps cover travel costs associated with larger scale competitions." },
+                    { amount: "$2500", description: "Helps us expand our programs for underserved athletes." },
+                    { amount: "$__", description: "Make a gift. Every donation, no matter the size, will help us make Boston strong  together." },
                     
                     ]}
                 />
