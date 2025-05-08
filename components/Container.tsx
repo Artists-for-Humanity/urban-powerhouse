bcfdb53b771333791c8b08aeb0667592481fcd32
Ctech 21M <tnguyen301@bostonk12.org>
2025-05-08 17:38:38 -0400

    Adding About Urban House Section and play with it a little bit

--- components/Container.tsx
+++ components/Container.tsx
@@ -14,11 +14,16 @@ const Container: React.FC<ContainerProps> = ({ children, className = '', header 
       }
     >
       {/* Header Section */}
-      {header && (
+      {header = "ABout Us" && (
         <div className="text-left mb-4 col-span-full">
           {header}
+          <div className="text-[70px] p-5 absolute text-white z-1">Thank you</div>
+          <video width="700px" height="700px" controls>
+            <source src="/logos/file.mp4"/></video> 
+          <div className="text-5xl ">About Urban Powerhouse</div>
         </div>
       )}
+        
 
       {/* Children */}
       {children}
