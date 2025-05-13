ec94b1c4e712ea7086a0f246ca4358711bef33b3
William <wfchen30@gmail.com>
2025-05-13 17:29:55 -0400

    Commit-Day#1: Failed inline css styling attempt

--- pages/FAQ.tsx
+++ pages/FAQ.tsx
@@ -6,15 +6,24 @@ import Container from "../components/Container";
 import Navigation from "../components/Navigation";
 import '../app/globals.css';
 
-
+function Question({ value, styleType }) {
+  return <div style={styleType} className="FAQ">Lorem Ipsum {value}</div>
+}
 export default function FAQ() {
-
  
 
-    return (
-          <Grid className="gap-y-4">
+    return (                     
+          <Grid className="gap-y-4 overflow-hidden">
             <Navigation/>
               <Container className="min-h-[60vh]">
+                <h1 className="title">FAQ</h1>
+                <Question value ="1" styleType={{grid-column-start: span 4}}/>
+                <Question value ="2"/>
+                <Question value ="3"/>
+                <Question value ="4"/>
+                
+
+                  
               </Container>
             <Footer />
           </Grid>
