03a91aa24332953017c5977e9303e780cba66564
brianh <162504882+brianh-002@users.noreply.github.com>
2025-05-14 17:59:12 -0400

    got the title and text in

---
+++ components/Newstitle.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import '../app/globals.css';
+export default function Newstitle() {
+
+    return (
+    <div className="col-start-3 col-span-3 text-[28px]">News%Events</div> 
+    );
+}
--- pages/News.tsx
+++ pages/News.tsx
@@ -5,15 +5,19 @@ import Footer from "../components/Footer";
 import Container from "../components/Container";
 import Navigation from "../components/Navigation";
 import '../app/globals.css';
-
+import Newstitle from "../components/Newstitle";
+import FirstArticle from "../components/FirstArticle";
 
 
 export default function News() {
 
     return (
-          <Grid>
+          <Grid className=" break-all">
             <Navigation/>
               <Container className="min-h-[60vh]">
+                <Newstitle></Newstitle>
+                
+                <FirstArticle></FirstArticle>
               </Container>
             <Footer />
           </Grid>
