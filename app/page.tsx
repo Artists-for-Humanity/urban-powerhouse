dc3d6df205ac0340204a031058198c5b69c0409e
Ctech 21M <tnguyen301@bostonk12.org>
2025-05-15 17:45:56 -0400

    Day 3

--- app/page.tsx
+++ app/page.tsx
@@ -17,8 +17,8 @@ export default function Home() {
           />
           <Container className=" mt-[20vh] min-h-[100vh]">
 
-        <div className=" mt-[40] mb-[40]!important justify-center relative items-center  font-[Poppins]  text-left mb-4 col-span-full font-semibold">
-          <div className="section-1 h-[788]">
+        <div className="justify-center relative items-center  font-[Poppins]  text-left mb-4 col-span-full font-semibold">
+          <div className="section-1 mt-[40] px-[4] h-[788]">
             <div className="video_Thanks w-full grid justify-center items-center">
             <div className="text-[4rem] absolute text-white w-full justify-center font-extrabold uppercase leading-none z-10 text-transparent bg-clip-text bg-center bg-cover bg-no-repeat">
               Giving <br /> Thanks
@@ -36,7 +36,7 @@ export default function Home() {
               <Button className=" w-[100] h-[40] absolute flex justify-center items-center" label="Read more" onClick={() => console.log('Sign In clicked')} variant="default" />
             </div>
           </div>
-          <div className="section-2">
+          <div className="section-2 mt-[40] px-[4]">
             <img src="quote.svg" alt="Your devide does not support this picture" className="mb-[40] relative w-[140.33] h-[111]" />
             <h3 className="text-[16px] w-full grid">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim  veniam, quis  </h3>
             <br />
@@ -45,12 +45,13 @@ export default function Home() {
               <img src="/headshots/founder1.png" alt="our devide does not support this picture" className="h-auto w-[240] mb-[40] rounded-full grayscale "/>
             </div>
           </div>
-          <div className="section-3">
+          <div className="section-3 mt-[40] px-[4]">
             <div className="title w-full justify-center flex relative">
               <div className="relative text-[32px] ">Our GYM</div>
             </div>
-            <img src="/gymshots/Championship2025.jpg" alt="" className="ComunityPicture-1 grid relative min-w-[100vw] m-0 h-[282]" />
           </div>
+          <div className="min-w-full mb-[40]"></div>
+            <img src="/gymshots/Championship2025.jpg" alt="" className="min-w-[100vw] object-cover" />
         </div>
            
             </Container>
