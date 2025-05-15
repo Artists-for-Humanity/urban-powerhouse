30a24d9ea10ddc75250bf38c3e4dce3e0fc312a2
ZAE42 <izayahfuller3@gmail.com>
2025-05-15 15:29:04 -0400

    figuring out the use of grid

--- pages/Donation.tsx
+++ pages/Donation.tsx
@@ -13,11 +13,14 @@ import '../app/globals.css';
 export default function Donation() {
 
     return (
-          <Grid className="gap-y-4">
+          <Grid className="gap-y-4 ">
             <Navigation/>
               <Container>
-              
+              <div className="grid grid-cols-2 place-content-center h-48 ...">
+              <div>Why Give to UPH?</div>
+              </div>
 
+              
               </Container>
             <Footer />
           </Grid>
