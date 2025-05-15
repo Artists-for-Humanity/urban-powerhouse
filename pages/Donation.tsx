9a372255c64c2aad383a6b8d269f7d81f12952d4
ZAE42 <izayahfuller3@gmail.com>
2025-05-15 17:43:35 -0400

    finished adding the text

--- pages/Donation.tsx
+++ pages/Donation.tsx
@@ -16,9 +16,10 @@ export default function Donation() {
           <Grid className="gap-y-4 ">
             <Navigation/>
               <Container>
-              <div className="grid grid-cols-2 place-content-center h-48 ...">
-              <div>Why Give to UPH?</div>
-              </div>
+          
+              <div className="col-span-6 text-center sm:col-start-4 text-3xl">Why Give to UPH?</div>
+             <div className="col-span-7 col-start-1 border-l-[13px] border-red-500 pt-12 pb-12 pl-10">Lorem ipsum dolor sit amet consectetur. Cras eget urna pellentesque diam tincidunt diam. Elementum imperdiet vitae sit consectetur pharetra. Platea phasellus cursus sed praesent eget scelerisque. Pretium leo vulputate facilisis elit rhoncus. Venenatis urna imperdiet velit at ultrices ultrices porttitor integer. Fringilla fames ultrices odio egestas dui etiam. Arcu fermentum mauris sit maecenas eu dolor justo ut ut. Tellus nam velit vitae vulputate nec vestibulum sed massa nibh. Hendrerit cras elementum volutpat magna in lorem enim.</div>
+            <div className="col-span-7 col-start-1 border-l-[13px] border-red-500 pt-12 pb-12 pl-10">Lorem ipsum dolor sit amet consectetur. Cras eget urna pellentesque diam tincidunt diam. Elementum imperdiet vitae sit consectetur pharetra. Platea phasellus cursus sed praesent eget scelerisque. Pretium leo vulputate facilisis elit rhoncus. Venenatis urna imperdiet velit at ultrices ultrices porttitor integer. Fringilla fames ultrices odio egestas dui etiam. Arcu fermentum mauris sit maecenas eu dolor justo ut ut. Tellus nam velit vitae vulputate nec vestibulum sed massa nibh. Hendrerit cras elementum volutpat magna in lorem enim.</div>              
 
               
               </Container>
