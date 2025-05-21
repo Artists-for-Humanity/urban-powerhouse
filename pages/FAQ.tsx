c44c56d5cc681fcd4d4faffc28ea644f83e20fe9
William <wfchen30@gmail.com>
2025-05-21 17:35:09 -0400

    Commit-day#4: Created container flex boxes, deleted useless row stuff, added new component for info, not finished with Onclick function

--- pages/FAQ.tsx
+++ pages/FAQ.tsx
@@ -8,47 +8,60 @@ import '../app/globals.css';
 
 
 export default function FAQ() {
+
 function Question({ text }) {
-  return <div className="col-start-1 col-span-6 FAQ">{text}</div>
+  return <div className="FAQ">{text}</div>
 }
 
 function MoreInfoButton({image}) {
-  return <button className="col-start-6 col-span-1 sm:col-start-8 lg:col-start-12 Click"></button>
+  return <button className="bg-{image} Click">\/</button>
 }
+
+function MoreInformation({ text, update }) {
+  return <div className="flex col-start-1 col-span-12 {update} Info">{text}</div>
+}
+
 
     return (                     
           <Grid className="gap-y-4 overflow-hidden break-all">
             <Navigation/>
               <Container className="min-h-[60vh] ">
                 <h1 className="text-[32px] col-span-11 title">FAQ</h1>
-                <div className="FAQ-Block-1">
+                <div className="flex place-content-between col-start-1 col-span-12 FAQ-Block-1">
                   <Question text = "Lorem Ipsum 1"/>
-                  <MoreInfoButton image = "3"/>
+                  <MoreInfoButton image = "arrowButton.png"/>
                 </div>
-                <div className="FAQ-Block-2">
+                <MoreInformation text = "geuigsoohs"/>
+                <div className="flex place-content-between col-start-1 col-span-12 FAQ-Block-2">
                   <Question text = "Lorem Ipsum 2"/>
-                  <MoreInfoButton image = "2"/>
+                  <MoreInfoButton image = "arrowButton.png"/>
                 </div>
-                <div className="FAQ-Block-3">
-                <Question text = "Lorem Ipsum 3"/>
-                <MoreInfoButton image = "2"/>
+                <MoreInformation text = "geuigsoohs"/>
+                <div className="flex place-content-between col-start-1 col-span-12 FAQ-Block-3">
+                  <Question text = "Lorem Ipsum 3"/>
+                  <MoreInfoButton image = "arrowButton.png"/>
                 </div>
-                <div className="FAQ-Block-4">
-                <Question text = "Lorem Ipsum 4"/>
-                <MoreInfoButton image = "2"/>
+                <MoreInformation text = "geuigsoohs"/>
+                <div className="flex place-content-between col-start-1 col-span-12 FAQ-Block-4">
+                  <Question text = "Lorem Ipsum 4"/>
+                  <MoreInfoButton image = "arrowButton.png"/>
                 </div>
-                <div className="FAQ-Block-5">
-                <Question text = "Lorem Ipsum 5"/>
-                <MoreInfoButton image = "2"/>
+                <MoreInformation text = "geuigsoohs"/>
+                <div className="flex place-content-between col-start-1 col-span-12 FAQ-Block-5">
+                  <Question text = "Lorem Ipsum 5"/>
+                  <MoreInfoButton image = "arrowButton.png"/>
                 </div>
-                <div className="FAQ-Block-6">
-                <Question text = "Lorem Ipsum 6"/>
-                <MoreInfoButton image = "2"/>
+                <MoreInformation text = "geuigsoohs"/>
+                <div className="flex place-content-between col-start-1 col-span-12 FAQ-Block-6">
+                  <Question text = "Lorem Ipsum 6"/>
+                  <MoreInfoButton image = "arrowButton.png"/>
                 </div>
-                <div className="FAQ-Block-7">
-                <Question text = "Lorem Ipsum 7"/>
-                <MoreInfoButton image = "2"/>
+                <MoreInformation text = "geuigsoohs"/>
+                <div className="flex place-content-between col-start-1 col-span-12 FAQ-Block-7">
+                  <Question text = "Lorem Ipsum 7"/>
+                  <MoreInfoButton image = "arrowButton.png"/>
                 </div>
+                <MoreInformation text = "geuigsoohs"/>
               
 
                   
