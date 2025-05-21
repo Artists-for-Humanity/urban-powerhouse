49b507fdb402ae758cf078f9491e85f3262979b7
ZAE42 <izayahfuller3@gmail.com>
2025-05-21 15:50:24 -0400

    added the text

--- pages/Donation.tsx
+++ pages/Donation.tsx
@@ -20,7 +20,7 @@ export default function Donation() {
               <div className="col-span-6 text-center sm:col-start-4 text-3xl">Why Give to UPH?</div>
              <div className="col-span-7 col-start-1 border-l-[13px] border-red-500 pt-12 pb-12 pl-10">Lorem ipsum dolor sit amet consectetur. Cras eget urna pellentesque diam tincidunt diam. Elementum imperdiet vitae sit consectetur pharetra. Platea phasellus cursus sed praesent eget scelerisque. Pretium leo vulputate facilisis elit rhoncus. Venenatis urna imperdiet velit at ultrices ultrices porttitor integer. Fringilla fames ultrices odio egestas dui etiam. Arcu fermentum mauris sit maecenas eu dolor justo ut ut. Tellus nam velit vitae vulputate nec vestibulum sed massa nibh. Hendrerit cras elementum volutpat magna in lorem enim.</div>
             <div className="col-span-7 col-start-1 border-l-[13px] border-red-500 pt-12 pb-12 pl-10">Lorem ipsum dolor sit amet consectetur. Cras eget urna pellentesque diam tincidunt diam. Elementum imperdiet vitae sit consectetur pharetra. Platea phasellus cursus sed praesent eget scelerisque. Pretium leo vulputate facilisis elit rhoncus. Venenatis urna imperdiet velit at ultrices ultrices porttitor integer. Fringilla fames ultrices odio egestas dui etiam. Arcu fermentum mauris sit maecenas eu dolor justo ut ut. Tellus nam velit vitae vulputate nec vestibulum sed massa nibh. Hendrerit cras elementum volutpat magna in lorem enim.</div>              
-
+            <div></div>
               
               </Container>
             <Footer />
