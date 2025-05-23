e55d2344fbbf77e8c5403c5e040477cc1c760f16
brianh <162504882+brianh-002@users.noreply.github.com>
2025-05-23 17:18:24 -0400

    Update News.tsx

--- pages/News.tsx
+++ pages/News.tsx
@@ -12,12 +12,13 @@ export default function News() {
         <Grid className="grid grid-rows-auto gap-y-8">
             <Navigation />
             <Container className="min-h-[60vh] grid gap-y-8">
-                <div className="col-start-3 col-span-3 text-[28px] ">News & Events</div> 
-                <div className="col-start-2 col-span-4 grid">
-                    <div className="text-[28px] font-bold text-left border-l-[10px] border-blue-800">
+                <div className="col-start-2 col-span-4 text-[28px] sm:col-start-2  ">News & Events</div>
+                <div className="text-[28px] font-bold text-left border-l-[10px] border-blue-800 col-start-2 col-span-4 sm:col-start-2 sm:col-span-6 ">
                     2024 City of Boston SucessLink Program
-                    </div>
-                    <p>
+                </div>
+                <div className="col-start-2 col-span-4 grid sm:col-span-6 sm:col-start-2 lg:col-start-2 lg:col-span-9">
+                    
+                    <p >
                         Urban PowerHouse is proud to be a Summer 2024 SuccessLink Employment partner with the City of Boston.
                     </p>
                     <br />
@@ -28,18 +29,42 @@ export default function News() {
                     <p>
                         We are looking forward to employing 11 youth workers and 1 young adult worker this summer. Interested applicants should complete this google form. Urban PowerHouse will reach out to candidates of interest to schedule an in-person or virtual interview.
                     </p>
+                    <br />
                 </div>
-                <div className=" col-start-2 col-span-4 gap-y-8">
+                <div className=" col-start-2 col-span-4 gap-y-8 sm:col-span-6 sm:col-start-2">
                     <img  className='' src="/Group-55.png" alt="Quote Icon" />
                     <div className="text-[24px] font-bold ">Lorem ipsum dolor sit amet consectetur. Vel posuere tristique sed diam. Cursus urna in in odio vulputate.</div>
+                    <br />
                 </div>
-                <div className="text-[16px] col-start-2 col-span-4">Lorem ipsum dolor sit amet consectetur. Vel posuere tristique sed diam. Cursus urna in in odio vulputate.. Lorem elementum eget nunc risus sed at mauris sit.. Integer in tortor volutpat dui. Id eget cras euismod sit nulla morbi etiam.. Ut pellentesque nibh quam vitae ut mi sed sem.. Placerat tincidunt varius at felis congue imperdiet.. Enim a elit risus amet tortor facilisi.. Faucibus eget quis pharetra quis metus enim imperdiet scelerisque sagittis..</div>
-                <div className=" col-start-2 col-span-4"><img src="/gymshots/Championship2025.jpg" alt="" /></div>
-                <div className=" col-start-2 col-span-4">Lorem ipsum dolor sit amet consectetur. Vel posuere tristique sed diam. Cursus urna in in odio vulputate.. Lorem elementum eget nunc risus sed at mauris sit.. Integer in tortor volutpat dui. Id eget cras euismod sit nulla morbi etiam.. Ut pellentesque nibh quam vitae ut mi sed sem.. Placerat tincidunt varius at felis congue imperdiet.. Enim a elit risus amet tortor facilisi.. Faucibus eget quis pharetra quis metus enim imperdiet scelerisque sagittis..</div>
-                <div className=" col-start-2 col-span-4">In neque mauris nunc eros mauris in congue sit.. Mauris id cras nunc adipiscing mauris ut eu dignissim elementum.. Scelerisque donec turpis ut arcu egestas ac rhoncus sed sit.. Posuere enim eget mauris et eu dui magna.. Quam consectetur sem eget ultrices donec risus velit sodales.. Cras integer iaculis gravida morbi aliquam eget senectus viverra.. Dictum phasellus fringilla massa sed viverra libero vitae nulla.. Dictum viverra sed pretium enim sollicitudin lorem in enim a.. Tortor quam vel tristique vestibulum mi accumsan odio faucibus aliquam.. Lorem nisl ut cras faucibus massa diam vitae consequat egestas.. Et nec ut cursus sit facilisi lobortis congue bibendum.. Amet et orci arcu id.</div>
-                <div className=" col-start-2 col-span-4 text-center" ><img src="/Youth Fest 2024.png" alt="" />look at this fun image</div>
-                <div className=" col-start-2 col-span-4"> Lorem ipsum dolor sit amet consectetur. Vel posuere tristique sed diam. Cursus urna in in odio vulputate.. Lorem elementum eget nunc risus sed at mauris sit.. Integer in tortor volutpat dui. Id eget cras euismod sit nulla morbi etiam.. Ut pellentesque nibh quam vitae ut mi sed sem.. Placerat tincidunt varius at felis congue imperdiet.. Enim a elit risus amet tortor facilisi.. </div>
-                <div className=" col-start-2 col-span-4">In neque mauris nunc eros mauris in congue sit.. Mauris id cras nunc adipiscing mauris ut eu dignissim elementum.. Scelerisque donec turpis ut arcu egestas ac rhoncus sed sit.. Posuere enim eget mauris et eu dui magna.. Quam consectetur sem eget ultrices donec risus velit sodales.. Cras integer iaculis gravida morbi aliquam eget senectus viverra.. </div>
+                <div className=" col-start-2 col-span-4 sm:col-span-6 sm:col-start-2">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur. Vel posuere tristique sed diam. Cursus urna in in odio vulputate.. Lorem elementum eget nunc risus sed at mauris sit.. Integer in tortor volutpat dui. Id eget cras euismod sit nulla morbi etiam.. Ut pellentesque nibh quam vitae ut mi sed sem.. Placerat tincidunt varius at felis congue imperdiet.. Enim a elit risus amet tortor facilisi.. Faucibus eget quis pharetra quis metus enim imperdiet scelerisque sagittis..
+                    </p>
+                    <br />
+                    <div className=" "><img src="/gymshots/Championship2025.jpg" alt="" /></div>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur. Vel posuere tristique sed diam. Cursus urna in in odio vulputate.. Lorem elementum eget nunc risus sed at mauris sit.. Integer in tortor volutpat dui. Id eget cras euismod sit nulla morbi etiam.. Ut pellentesque nibh quam vitae ut mi sed sem.. Placerat tincidunt varius at felis congue imperdiet.. Enim a elit risus amet tortor facilisi.. Faucibus eget quis pharetra quis metus enim imperdiet scelerisque sagittis..
+                    </p>
+                    <br />
+                    <p>
+                        In neque mauris nunc eros mauris in congue sit.. Mauris id cras nunc adipiscing mauris ut eu dignissim elementum.. Scelerisque donec turpis ut arcu egestas ac rhoncus sed sit.. Posuere enim eget mauris et eu dui magna.. Quam consectetur sem eget ultrices donec risus velit sodales.. Cras integer iaculis gravida morbi aliquam eget senectus viverra.. Dictum phasellus fringilla massa sed viverra libero vitae nulla.. Dictum viverra sed pretium enim sollicitudin lorem in enim a.. Tortor quam vel tristique vestibulum mi accumsan odio faucibus aliquam.. Lorem nisl ut cras faucibus massa diam vitae consequat egestas.. Et nec ut cursus sit facilisi lobortis congue bibendum.. Amet et orci arcu id.
+                    </p>
+                    <br />
+                </div>
+                <div className="col-start-2 col-span-4 sm:col-span-6 sm:col-start-2 text-center">
+                    <img className="mx-auto" src="/Youth Fest 2024.png" alt="Youth Fest 2024" />
+                    <p>look at this fun image</p>
+                </div>
+                <div className=" col-start-2 col-span-4 sm:col-span-6 sm:col-start-2 ">
+                    <br />
+                    <p> 
+                        Lorem ipsum dolor sit amet consectetur. Vel posuere tristique sed diam. Cursus urna in in odio vulputate.. Lorem elementum eget nunc risus sed at mauris sit.. Integer in tortor volutpat dui. Id eget cras euismod sit nulla morbi etiam.. Ut pellentesque nibh quam vitae ut mi sed sem.. Placerat tincidunt varius at felis congue imperdiet.. Enim a elit risus amet tortor facilisi.. 
+                    </p>
+                    <br />
+                    <p>
+                        In neque mauris nunc eros mauris in congue sit.. Mauris id cras nunc adipiscing mauris ut eu dignissim elementum.. Scelerisque donec turpis ut arcu egestas ac rhoncus sed sit.. Posuere enim eget mauris et eu dui magna.. Quam consectetur sem eget ultrices donec risus velit sodales.. Cras integer iaculis gravida morbi aliquam eget senectus viverra.. 
+                    </p>
+                </div>
                 <Button label="Go Back"className="mt-4 col-start-3 col-span-2 ">Click Me</Button>            
             </Container>
             <Footer />
