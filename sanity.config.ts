14b4ec2c2ea9de890a52d0d9e2a87f1b6a241421
Jamie <itisjamied@gmail.com>
2025-05-29 11:31:31 -0400

    Add sanity-plugin-media to enhance media handling in Sanity Studio
    - Updated package.json to include "sanity-plugin-media" version 3.0.2.
    - Modified sanity.config.ts to integrate the media plugin into the Sanity Studio configuration.
    - Uploaded pixiset photo album to Sanity

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,6 +1,7 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
+import {media} from 'sanity-plugin-media'
 import {schemaTypes} from './schemaTypes'
 
 export default defineConfig({
@@ -12,7 +13,7 @@ export default defineConfig({
 
   basePath: '/studio',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [structureTool(), visionTool(), media()],
 
   schema: {
     types: schemaTypes,
