71c54792430bd59c600cd8b96e69f29d60fd5367
Mohamed <thatkidm2@gmail.com>
2025-06-03 17:51:57 -0400

    mobile finished

--- pages/News.tsx
+++ pages/News.tsx
@@ -15,24 +15,25 @@ export default function News() {
    <Container className=" bg-black px-2 py-10">
    <div className="pb-[35] text-leftcol-start-2 col-span-6 font-style: Poppinsl text-[32px] pb-10;">News & Events</div>
 
-    <div className="col-start-2 col-span-8 text-white p-8 mb-10 text-sm font-bold mb-4 text-[20px] lg:mb-0
- pl-[0px] lg:pl-0 lg:text-[32px] lg:col-start-2 lg:col-span-10"> 2024 City of Boston SuccessLink Program
+    <div className="col-start-2 col-span-8 text-white m-8 mb-10 text-sm font-bold mb-4 text-[20px] lg:mb-10 border-l-[6px] border-blue-700
+
+ pl-[10px] ml-[0px] lg:pl-0 lg:text-[32px] lg:col-start-2 lg:col-span-10"> 2024 City of Boston SuccessLink Program
       </div>
-      <p className="col-start-2 col-span-8 text-sm mb-3"> 
+      <p className="col-start-2 col-span-8 text-sm mb-3 text-[16px]lg:col-span-2lg:col-span-9 text-[16px]"> 
       Urban Powerhouse is proud to be a Summer 2024 SuccessLink Employment partner with the City of Boston.
       </p>
-      <p className="col-start-2 col-span-8 text-sm mb-3" > 
+      <p className="col-start-2 col-span-8 text-sm mb-3 lg:col-start-2 lg:col-span-9 text-[16px]" > 
         The Office of Youth Employment and Opportunity is supporting Boston-area 501(c)(3) nonprofits interested in providing  funding for employment opportunities to youth participants between the  ages of 14-18, and young adults between the ages of 19-24.
       </p>
-      <p className="col-start-2 col-span-8 text-sm mb-3"> 
+      <p className="col-start-2 col-span-8 text-sm mb-5 lg:col-start-2 lg:col-span-8 text-[16px]"> 
         We are looking forward to employing 11 youth workers and 1 young adult  worker this summer. Interested applicants should complete this google form. Urban PowerHouse will reach out to candidates of interest to schedule an in-person or virtual interview.
-      </p>
+      </p><Button label="Read More"className="col-start-4 col-span-6 mb-5 pl-[8px]"></Button>
 
 
       
       
-     <div className="col-start-2 col-span-8 text-white p-8 mb-10 text-sm font-bold mb-4 text-[20px] lg:mb-0
- pl-[0px] lg:pl-0 lg:text-[32px] lg:col-start-2 lg:col-span-10">   
+     <div className="col-start-2 col-span-8 text-white m-8 mb-10 text-sm font-bold mb-4 text-[20px] ml-0 lg:ml-6 border-l-[6px] border-blue-700
+ pl-[10px] lg:pl-0 lg:text-[32px] lg:col-start-2 lg:col-span-10">   
      2024 Lorem ipsum dolor sit amet consectetur. Gravida.
       </div>
       <p className="col-start-2 col-span-9 text-sm mb-6 text-[16px]"> 
@@ -43,17 +44,18 @@ export default function News() {
       </p>
 
 
-      <div className="col-start-2 col-span-8 text-white p-8 mb-10 text-sm font-bold mb-4 text-[20px] lg:mb-0
- pl-[0px] lg:pl-0 lg:text-[32px] lg:col-start-2 lg:col-span-10">
+      <div className="col-start-2 col-span-8 text-white m-8 ml-0 text-sm font-bold mb-4 text-[20px] border-l-[6px] border-blue-700
+ lg:pl-0 lg:text-[32px] lg:col-start-2 lg:col-span-10  pl-[10px]">
         2023 Lmet consectetur. Gravida.   
       </div>
       <p className="col-start-2 col-span-10 text-sm mb-3 text-[16px]">
         Lorem ipsum dolor sit amet consectetur. Vel posuere tristique sed diam. Cursus urna in in odio vulputate.. Lorem elementum eget nunc risus sed at mauris sit.. Integer in tortor volutpat dui. Id eget cras euismod sit nulla morbi etiam.. Ut pellentesque nibh quam vitae ut mi sed sem.. Placerat tincidunt varius at felis congue imperdiet.. Enim a elit risus amet tortor facilisi..  
       </p>
-      <p className="col-start-2 col-span-9 text-sm mb-3 text-[16px]" >
+      <p className="col-start-2 col-span-9 text-sm mb-7 text-[16px]" >
         In neque mauris nunc eros mauris in congue sit.. Mauris id cras nunc adipiscing mauris ut eu dignissim elementum.. Scelerisque donec turpis ut arcu egestas ac rhoncus sed sit.. Posuere enim eget mauris et eu dui magna.. Quam consectetur sem eget ultrices donec risus velit sodales.. Cras integer iaculis gravida morbi aliquam eget senectus viverra..  
-      </p>
-      <Button label="Show More"className="lg:col-start-6 col-span-2 mb-6 pl-[8px]"></Button>
+      </p><Button label="Read More"className="col-start-4 col-span-7 mb-15 pl-[8px]"></Button>
+
+      <Button label="Show More"className="col-start-3 col-span-2 mb-6 pl-[8px]"></Button>
 
 
 
