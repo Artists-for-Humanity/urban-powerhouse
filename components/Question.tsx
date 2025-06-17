b8e5cb0fe36dc68271b6c9bc4ba69e1e0174f967
William <wfchen30@gmail.com>
2025-06-17 15:41:53 -0400

    on click function attempt

--- components/Question.tsx
+++ components/Question.tsx
@@ -4,18 +4,14 @@ import Image from 'next/image';
 interface QuestionProps{
       faq?: string
       answer?: string
-      infoDisplay?: string
+      hidden?: string
+      click?: any
 }
 
 
 
-  const Question: React.FC<QuestionProps> = ({faq, answer, infoDisplay}) => {
-      const values = ['hidden', ''];
-      const handleClick = () => {
-         console.log("clicked")
-         const 
-      }
-      
+  const Question: React.FC<QuestionProps> = ({faq, answer, hidden, click}) => {
+   
       return (
         <div className="flex-column col-start-1 col-span-12 bg-c pt-[0.8rem] pb-[0.8rem] sm:pt-[1.2rem] sm:pb-[1.2rem] lg:pt-[1.5rem] lg:pb-[1.5rem]">
             <div className="flex place-content-between col-start-1 col-span-12 font-semibold">
@@ -28,11 +24,11 @@ interface QuestionProps{
                   width={1000}
                   height={1000}
                   alt="button"
-                  onClick = {handleClick}
+                  onClick={click}
                   />
                </div>
             </div>
-            <div className="flex pl-[0.4rem] pr-[0.4rem] sm:pl-[0.6rem] sm:pr-[0.6rem] lg:pl-[1rem] lg:pr-[1rem] {infoDisplay}">
+            <div className="flex pl-[0.4rem] pr-[0.4rem] sm:pl-[0.6rem] sm:pr-[0.6rem] lg:pl-[1rem] lg:pr-[1rem] {hidden} box">
             {answer}
             </div>
          </div>
