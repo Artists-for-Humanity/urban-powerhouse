56ffde068a1992e896d83adcbebf253dab75da65
ZAE42 <izayahfuller3@gmail.com>
2025-06-17 16:15:22 -0400

    finished everything

--- pages/Donation.tsx
+++ pages/Donation.tsx
@@ -16,55 +16,74 @@ export default function Donation() {
           <Grid className="gap-y-4 ">
             <Navigation/>
               <Container>
-          
               <div className="col-span-6 text-center sm:col-start-4 lg:col-start-1 lg:col-span-4 text-3xl">Why Give to UPH?</div>
-              <div className="max-h-26 lg:col-start-3 lg:mt-5 lg:col-span-8 sm:max-h-full col-span-7 col-start-1 border-l-[13px] border-red-500 mt-12 mb-65 ml-10 sm:mt-0 sm:mb-0 sm:ml-0 sm:col-start-2 sm:col-span-6 sm:mb-10">Lorem ipsum dolor sit amet consectetur. Cras eget urna pellentesque diam tincidunt diam. Elementum imperdiet vitae sit consectetur pharetra. Platea phasellus cursus sed praesent eget scelerisque. Pretium leo vulputate facilisis elit rhoncus. Venenatis urna imperdiet velit at ultrices ultrices porttitor integer. Fringilla fames ultrices odio egestas dui etiam. Arcu fermentum mauris sit maecenas eu dolor justo ut ut. Tellus nam velit vitae vulputate nec vestibulum sed massa nibh. Hendrerit cras elementum volutpat magna in lorem enim.</div>
-              <div className="max-h-26 lg:col-start-3 lg:mt-5 lg:col-span-8 sm:max-h-full col-span-7 col-start-1 border-l-[13px] border-red-500 mt-12 mb-65 ml-10  sm:mt-0 sm:mb-0 sm:ml-0 sm:col-start-2 sm:col-span-6 sm:mb-10">Lorem ipsum dolor sit amet consectetur. Cras eget urna pellentesque diam tincidunt diam. Elementum imperdiet vitae sit consectetur pharetra. Platea phasellus cursus sed praesent eget scelerisque. Pretium leo vulputate facilisis elit rhoncus. Venenatis urna imperdiet velit at ultrices ultrices porttitor integer. Fringilla fames ultrices odio egestas dui etiam. Arcu fermentum mauris sit maecenas eu dolor justo ut ut. Tellus nam velit vitae vulputate nec vestibulum sed massa nibh. Hendrerit cras elementum volutpat magna in lorem enim.</div>              
-              <div className="col-span-4 col-start-2 sm:col-span-2 sm:col-start-2 text-center flex flex-col items-center">
-                <div className="w-32 h-32 bg-gray-600 text-[30px] rounded-full flex items-center justify-center">$30</div>
-                <div className="max-h-26 mt-5">Become a Friend of UPH</div>
-              </div>
+              <div className="max-h-25 lg:col-start-3 lg:mt-5 lg:col-span-8 pl-4 lg:max-h-20 lg:mb-30 sm:max-h-20 col-span-7 col-start-1 border-l-[13px] border-orange-500 mt-12 sm:mb-30 ml-10 mb-65  sm:mt-0 sm:mb-0 sm:ml-0 sm:col-start-2 sm:col-span-6 sm:mb-10">Lorem ipsum dolor sit amet consectetur. Cras eget urna pellentesque diam tincidunt diam. Elementum imperdiet vitae sit consectetur pharetra. Platea phasellus cursus sed praesent eget scelerisque. Pretium leo vulputate facilisis elit rhoncus. Venenatis urna imperdiet velit at ultrices ultrices porttitor integer. Fringilla fames ultrices odio egestas dui etiam. Arcu fermentum mauris sit maecenas eu dolor justo ut ut. Tellus nam velit vitae vulputate nec vestibulum sed massa nibh. Hendrerit cras elementum volutpat magna in lorem enim.</div>
+              <div className="max-h-25  lg:col-start-3 lg:mt-5 lg:col-span-8 pl-4 lg:max-h-20 lg:mb-30 sm:max-h-20 col-span-7 col-start-1 border-l-[13px] border-orange-500 mt-12 sm:mb-35 mb-80 ml-10  sm:mt-0 sm:mb-0 sm:ml-0 sm:col-start-2 sm:col-span-6 sm:mb-10">Lorem ipsum dolor sit amet consectetur. Cras eget urna pellentesque diam tincidunt diam. Elementum imperdiet vitae sit consectetur pharetra. Platea phasellus cursus sed praesent eget scelerisque. Pretium leo vulputate facilisis elit rhoncus. Venenatis urna imperdiet velit at ultrices ultrices porttitor integer. Fringilla fames ultrices odio egestas dui etiam. Arcu fermentum mauris sit maecenas eu dolor justo ut ut. Tellus nam velit vitae vulputate nec vestibulum sed massa nibh. Hendrerit cras elementum volutpat magna in lorem enim.</div>              
+             
+             
+                          <div className="col-start-2 col-span-4  sm:col-start-3 sm:col-span-4 lg:col-start-2 lg:col-span-10 ">
+                            
+                            <div className="sm:flex sm:flex-col sm:justify-center sm:items-center  lg:flex lg:flex-row lg:justify-evenly">
 
-                
-              <div className="col-span-4 col-start-2 lg:gap-y-4 gap-x-8 sm:col-span-2 sm:row-start-4 sm:col-start-4 text-center flex flex-col items-center">
-                <div className="w-32 h-32 bg-gray-600 text-[30px] rounded-full flex items-center justify-center bg-sky-600">$65</div>
-                <div className="max-h-26 mt-5">Supports a 12 month federation membership for one athlete</div>
-              </div>
-
-              <div className="col-span-4 col-start-2 sm:col-span-2 sm:row-start-4 sm:col-start-6 text-center flex flex-col items-center">
-              <div className="w-32 h-32 bg-gray-600 text-[30px] rounded-full flex items-center justify-center bg-sky-600"> $100 </div>
+                              <div className="lg:w-1/4 flex flex-col items-center">
+                                <div className="w-32 h-32 bg-gray-600 text-[30px] rounded-full flex items-center justify-center bg-gray-600">$30</div>
+                                <div className="max-h-26 text-center mt-5 mb-10">Become a Friend of UPH </div>
+                              </div>
+                              <div className="lg:w-1/4 flex flex-col items-center">
+                              <div className="w-32 h-32 bg-gray-600 text-[30px] rounded-full flex items-center justify-center bg-gray-600">$65</div>
+                              <div className="max-h-26 text-center col-span-4 col-start-2  mt-5 mb-10">Supports a 12 month federation membership for one athlete</div>
+                              </div>
+                              <div className="lg:w-1/4 flex flex-col items-center">
+                              <div className="w-32 h-32 bg-gray-600 text-[30px] rounded-full flex items-center justify-center bg-gray-600"> $100 </div>
               <div className="max-h-26 text-center col-span-4 col-start-2  mt-5 mb-10">Covers one competition entry fee for one athlete </div>
-              </div>
-                 
-              <div className="col-span-4 col-start-2 lg:col-start-8 lg:row-start-4 sm:col-span-2 sm:row-start-5 sm:col-start-2 text-center flex flex-col items-center">
-              <div className="w-32 h-32 bg-gray-600 text-[30px] rounded-full flex items-center justify-center bg-sky-600">$250</div>
-              <div className="max-h-26 text-center col-span-4 col-start-2  mt-5 mb-10">Covers approved competition gear for one athlete</div>
-              </div>
+                              </div>
+                              <div className="lg:w-1/4 flex flex-col items-center">
+                              <div className="w-32 h-32 bg-gray-600 text-[30px] rounded-full flex items-center justify-center bg-gray-600">$250</div>
+                              <div className="max-h-26 text-center col-span-4 col-start-2  mt-5 mb-10">Covers approved competition gear for one athlete</div>
+                              </div>
 
-              <div className="col-span-4 col-start-2 lg:col-start-2 lg:row-start-5 sm:col-span-2 sm:row-start-5 sm:col-start-4 text-center flex flex-col items-center">
-              <div className="w-32 h-32 bg-gray-600 text-[30px] rounded-full flex items-center justify-center bg-sky-600">$600</div>
+                            </div>
+
+             
+
+                            <div className="sm:flex sm:flex-col sm:justify-center sm:items-center lg:flex-row lg:flex lg:justify-evenly">
+
+             
+                            <div className="lg:w-1/4 flex flex-col items-center ">
+              <div className="text-center flex flex-col sm:pb-6 items-center">
+              <div className="w-32 h-32 bg-gray-600 text-[30px] rounded-full flex items-center justify-center bg-gray-600">$600</div>
               <div className="max-h-26 text-center col-span-4 col-start-2  mt-5 mb-10">Purchases a new barbell for athletes to use during training</div>
               </div>
-
+              </div>
               
 
-                <div className="col-span-4 col-start-2 lg:col-start-4 sm:col-span-2 sm:row-start-5 sm:col-start-6 text-center flex flex-col items-center">
-              <div className="w-32 h-32 bg-gray-600 text-[30px] rounded-full flex items-center justify-center bg-sky-600">$1000</div>
+              <div className="lg:w-1/4 flex flex-col items-center">
+                <div className="text-center flex flex-col sm:pb-6 items-center">
+              <div className="w-32 h-32 bg-gray-600 text-[30px] rounded-full flex items-center justify-center bg-gray-600">$1000</div>
               <div className="max-h-26 text-center col-span-4 col-start-2  mt-5 mb-10">Helps cover travel cost associated with larger scale competitions </div>
                 </div>
+              </div>
 
-                <div className="col-span-4 col-start-2 lg:row-start-5 lg:col-start-6 sm:col-span-2 sm:row-start-6 sm:col-start-5 text-center flex flex-col items-center">
-              <div className="w-32 h-32 bg-gray-600 text-[30px] rounded-full flex items-center justify-center bg-sky-600">$2500</div>
+
+              <div className="lg:w-1/4 flex flex-col items-center">
+                <div className="text-center flex flex-col sm:pb-6 items-center">
+              <div className="w-32 h-32 bg-gray-600 text-[30px] rounded-full flex items-center justify-center bg-gray-600">$2500</div>
               <div className="max-h-26 text-center col-span-4 col-start-2  mt-5 mb-10">Helps us open the doors to our first brick and mortar facility </div>
                 </div>
+              </div>
 
 
-                <div className="col-span-4 col-start-2 lg:row-start-5 lg:col-start-8 sm:col-span-2 sm:row-start-6 sm:col-start-3 text-center flex flex-col items-center">
-              <div className="w-32 h-32 bg-gray-600 text-[30px] rounded-full flex items-center justify-center bg-sky-600">$__</div>
+              <div className="lg:w-1/4 flex flex-col items-center">
+                <div className="text-center flex flex-col sm:pb-6  items-center">
+              <div className="w-32 h-32 bg-gray-600 text-[30px] rounded-full flex items-center justify-center bg-gray-600">$__</div>
               <div className="max-h-26 text-center col-span-4 col-start-2  mt-5 mb-10">Make a gift. Every donation, no matter the size, will help us make Boston strong  together</div>
                 </div>
+
+              </div>
+                            </div>
               
 
+                </div>
               </Container>
             <Footer />
           </Grid>
