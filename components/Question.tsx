7d4d0c1c7dcde4ad4ef9557d705b6de3cccaeca1
William <wfchen30@gmail.com>
2025-06-17 17:44:37 -0400

    fixed hidden function

--- components/Question.tsx
+++ components/Question.tsx
@@ -4,13 +4,28 @@ import Image from 'next/image';
 interface QuestionProps{
       faq?: string
       answer?: string
-      hidden?: string
-      click?: any
 }
 
+const values = [true, false]
+let i = 0
+let hidden = values[i]
+const handleClick = () => {
+      if (i == 0) {
+         i = 1
+         console.log("next click: hidden")
+         hidden = values[1]
+            
+      } else {
+            i = 0
+            console.log("next click: unhidden")
+            hidden = values[0]
+         }
+      console.log("clicked")
 
+   }
+   
 
-  const Question: React.FC<QuestionProps> = ({faq, answer, hidden, click}) => {
+   const Question: React.FC<QuestionProps> = ({faq, answer}) => {
    
       return (
         <div className="flex-column col-start-1 col-span-12 bg-c pt-[0.8rem] pb-[0.8rem] sm:pt-[1.2rem] sm:pb-[1.2rem] lg:pt-[1.5rem] lg:pb-[1.5rem]">
@@ -24,11 +39,11 @@ interface QuestionProps{
                   width={1000}
                   height={1000}
                   alt="button"
-                  onClick={click}
+                  onClick={handleClick}
                   />
                </div>
             </div>
-            <div className="flex pl-[0.4rem] pr-[0.4rem] sm:pl-[0.6rem] sm:pr-[0.6rem] lg:pl-[1rem] lg:pr-[1rem] {hidden} box">
+            <div className={`${hidden ? 'hidden' : ''} flex pl-[0.4rem] pr-[0.4rem] sm:pl-[0.6rem] sm:pr-[0.6rem] lg:pl-[1rem] lg:pr-[1rem] `}>
             {answer}
             </div>
          </div>
